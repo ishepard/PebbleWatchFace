e560869694f0053a2282fba1e529c81107ae07b1
ishepard <spadini.davide@gmail.com>
2015-02-17 18:58:12 +0100

    Now the street is showed..bug fix

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -4,28 +4,31 @@ function locationSuccess(pos) {
   var response;
   var response_city;
   var city;
-  var country;
+  var countrycode;
+  var street;
   console.log("lat:" + pos.coords.latitude + " long:" + pos.coords.longitude);
 
-  var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + pos.coords.latitude + "&lon=" + pos.coords.longitude;
-  request_city.open('GET', url, false);
+  var yahoo_get_city = encodeURIComponent("select countrycode, city, street from geo.placefinder where text=\"" + pos.coords.latitude + "," + pos.coords.longitude + "\" and gflags=\"R\"");
+  var yahoo_city = "https://query.yahooapis.com/v1/public/yql?q=" + yahoo_get_city + "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+  request_city.open('GET', yahoo_city , false);
   request_city.send();
   if (request_city.readyState == 4) {
       if(request_city.status == 200) {
         // console.log("request_city.RESPONSETEXT: " + request_city.responseText);
         response_city = JSON.parse(request_city.responseText);
-        city = response_city.name;
-        country = response_city.sys.country;
-
+        city = response_city.query.results.Result.city;
+        countrycode = response_city.query.results.Result.countrycode;
+        street = response_city.query.results.Result.street;
+        
       } else {
         console.log("Error");
       }
   }
   console.log("city: " + city);
-  console.log("country: " + country);
-  
+  console.log("countrycode: " + countrycode);
+  console.log("street: " + street);
 
-  var yahoo_weather_query = encodeURIComponent("select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text=\"" + city + "," + country + "\")");
+  var yahoo_weather_query = encodeURIComponent("select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text=\"" + city + "," + countrycode + "\")");
   var yahoo_weather = "https://query.yahooapis.com/v1/public/yql?q=" + yahoo_weather_query + "%20and%20u%3D%22c%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
   // console.log("yahoo_weather: " + yahoo_weather);
   req.open('GET', yahoo_weather, true);
@@ -44,8 +47,7 @@ function locationSuccess(pos) {
         var dictionary = {
         "KEY_TEMPERATURE": temperature,
         "KEY_CONDITIONS": conditions,
-        "KEY_CITY": city,
-        "KEY_COUNTRY": country
+        "KEY_STREET": street
       };
       
       // Send to Pebble
--- src/main.c
+++ src/main.c
@@ -2,19 +2,20 @@
 
 #define KEY_TEMPERATURE 0
 #define KEY_CONDITIONS 1
-#define KEY_CITY 2
-#define KEY_COUNTRY 3
+#define KEY_STREET 2
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 static TextLayer *s_date_layer;
 static TextLayer *s_weather_layer;
+static TextLayer *s_street_layer;
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
 
 static GFont s_time_font;
 static GFont s_date_font;
 static GFont s_weather_font;
+static GFont s_street_font;
 
 static void update_time() {
   // Get a tm structure
@@ -43,12 +44,13 @@ static void update_time() {
 
 static void main_window_load(Window *window) {
   // Create GFont
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_CLOCK_PROVA_65));
-  s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_CLOCK_20));
-  s_weather_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_CLOCK_PROVA_30));
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_CLOCK_65));
+  s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_CLOCK_ITALIC_20));
+  s_street_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ARIAL_20));
+  s_weather_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_CLOCK_30));
 
   // Create time TextLayer
-  s_time_layer = text_layer_create(GRect(0, 20, 144, 70));
+  s_time_layer = text_layer_create(GRect(0, 15, 144, 70));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorWhite);
   text_layer_set_text(s_time_layer, "00:00");
@@ -60,14 +62,20 @@ static void main_window_load(Window *window) {
   text_layer_set_text(s_date_layer, "00 aaa");
   
   // Create weather TextLayer
-  s_weather_layer = text_layer_create(GRect(75, 110, 69, 50));
+  s_weather_layer = text_layer_create(GRect(75, 100, 69, 50));
   text_layer_set_background_color(s_weather_layer, GColorClear);
   text_layer_set_text_color(s_weather_layer, GColorWhite);
-  text_layer_set_text(s_weather_layer, "Load");
+  text_layer_set_text(s_weather_layer, "");
+  
+  // Create date TextLayer
+  s_street_layer = text_layer_create(GRect(0, 150, 144, 20));
+  text_layer_set_background_color(s_street_layer, GColorClear);
+  text_layer_set_text_color(s_street_layer, GColorWhite);
+  text_layer_set_text(s_street_layer, "Yahoo Weather");
   
   // Create GBitmap, then set to created BitmapLayer
   s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_ERROR);
-  s_background_layer = bitmap_layer_create(GRect(10, 98, 60, 60));
+  s_background_layer = bitmap_layer_create(GRect(10, 85, 60, 60));
   bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
 
   // Apply to TextLayer
@@ -78,11 +86,13 @@ static void main_window_load(Window *window) {
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
   text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
+  text_layer_set_text_alignment(s_street_layer, GTextAlignmentCenter);
 
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_street_layer));
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
 
   // Make sure the time is displayed from the start
@@ -94,6 +104,7 @@ static void main_window_unload(Window *window) {
   text_layer_destroy(s_time_layer);
   text_layer_destroy(s_date_layer);
   text_layer_destroy(s_weather_layer);
+  text_layer_destroy(s_street_layer);
   bitmap_layer_destroy(s_background_layer);
   
   // Destroy GBitmap
@@ -103,7 +114,7 @@ static void main_window_unload(Window *window) {
   fonts_unload_custom_font(s_time_font);
   fonts_unload_custom_font(s_date_font);
   fonts_unload_custom_font(s_weather_font);
-
+  fonts_unload_custom_font(s_street_font);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -123,12 +134,65 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   }
 }
 
+static void set_bitmap_from_condition_code(int condition_code){
+    if (condition_code == 30){
+      s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PARTLY_CLOUDY_DAY);
+      bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  } else if (condition_code == 29){
+      s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PARTLY_CLOUDY_NIGHT);
+      bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  } else if ((condition_code >= 26 && condition_code <= 28) || condition_code == 44) {
+      s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_CLOUDY);
+      bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  } else if (condition_code == 31 || condition_code == 33) {
+      s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_CLEAR_NIGHT);
+      bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  } else if (condition_code == 32 || condition_code == 34 || condition_code == 36) {
+      s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_CLEAR_DAY);
+      bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  } else if (
+        (condition_code >= 41 && condition_code <= 43) || condition_code == 16 ||
+        condition_code == 13 || condition_code == 15 || condition_code == 25 || 
+        condition_code == 46
+      ){
+      s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_SNOW);
+      bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  } else if (condition_code >= 19 && condition_code <= 22){
+      s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_FOG);
+      bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  } else if ( 
+        condition_code == 3 || condition_code == 4 || condition_code == 9 ||
+        condition_code == 11 || condition_code == 12 || condition_code == 40 ||
+        condition_code == 45 || condition_code == 47
+      ){
+      s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_RAIN);
+      bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  } else if (
+        condition_code == 23 || condition_code == 24 || (condition_code >= 0 && condition_code <= 2) ||
+        (condition_code >= 37 && condition_code <= 39)
+      ){
+      s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_WIND);
+      bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  } else if ( 
+        (condition_code >= 5 && condition_code <= 8) || condition_code == 10 || 
+        condition_code >= 14 || condition_code <= 17 || condition_code == 18 || 
+        condition_code == 35
+      ){
+      s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_SLEET);
+      bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  } else if (condition_code == 3200){
+    s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_ERROR);
+    bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  }
+}
+
 static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
   // Store incoming information
   static char temperature_buffer[8];
   static char conditions_buffer[8];
   static char weather_layer_buffer[32];
-  int condition_code = 0;
+  static char street_buffer[32];
+  int condition_code = 3200;
   
   // Read first item
   Tuple *t = dict_read_first(iterator);
@@ -138,15 +202,14 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
     // Which key was received?
     switch(t->key) {
     case KEY_TEMPERATURE:
-      snprintf(temperature_buffer, sizeof(temperature_buffer), "%dC", (int)t->value->int32);
+      snprintf(temperature_buffer, sizeof(temperature_buffer), "%d C", (int)t->value->int32);
       break;
     case KEY_CONDITIONS:
       condition_code = (int)t->value->int32;
       snprintf(conditions_buffer, sizeof(conditions_buffer), "%d", (int)t->value->int32);
       break;
-    case KEY_CITY:
-      break;
-    case KEY_COUNTRY:
+    case KEY_STREET:
+      snprintf(street_buffer, sizeof(street_buffer), "%s", t->value->cstring);
       break;
     default:
       APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
@@ -156,41 +219,13 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
     // Look for next item
     t = dict_read_next(iterator);
   }
-  if (condition_code == 30){
-      s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PARTLY_CLOUDY_DAY);
-      bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
-  } else if (condition_code == 29){
-      s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PARTLY_CLOUDY_NIGHT);
-      bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
-  } else if (condition_code >= 26 && condition_code <= 28) {
-      s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_CLOUDY);
-      bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
-  } else if (condition_code == 31 || condition_code == 33) {
-      s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_CLEAR_NIGHT);
-      bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
-  } else if (condition_code == 32 || condition_code == 34) {
-      s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_CLEAR_DAY);
-      bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
-  } else if ((condition_code >= 41 && condition_code <= 43) || condition_code == 16) {
-      s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_SNOW);
-      bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
-  } else if (condition_code == 20){
-      s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_FOG);
-      bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
-  } else if ((condition_code >= 8 && condition_code <= 12) || condition_code == 35) {
-      s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_RAIN);
-      bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
-  } else if (condition_code == 24 || (condition_code >= 0 && condition_code <= 4)){
-      s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_WIND);
-      bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
-  } else if ( (condition_code >= 5 && condition_code <= 7) || (condition_code >= 13 && condition_code <= 15)){
-      s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_SLEET);
-      bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
-  }
+
+  set_bitmap_from_condition_code(condition_code);
   
   // Assemble full string and display
   snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s", temperature_buffer);
   text_layer_set_text(s_weather_layer, weather_layer_buffer);
+  text_layer_set_text(s_street_layer, street_buffer);
   
 }
 
